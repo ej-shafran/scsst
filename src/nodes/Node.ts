05c0444775a3566a75b2004659b9102a492e236b
ej-shafran <ejshafran@hilma.tech>
2023-04-30 02:54:57 +0300

    Rebuild Node type

--- src/nodes/Node.ts
+++ src/nodes/Node.ts
@@ -1,7 +1,21 @@
-import { Loc } from "../tokenize";
+import { Block } from "./Block";
+import { Comment } from "./Comment";
+import { Declaration } from "./Declaration";
+import { FunctionCall } from "./FunctionCall";
+import { MediaQuery } from "./MediaQuery";
+import { Rule } from "./Rule";
+import { Selector } from "./Selector";
+import { SelectorList } from "./SelectorList";
+import { SelectorSection } from "./SelectorSection";
 
-export type Node = {
-  readonly type: string;
-  loc: Loc;
-  children?: (Node | string)[];
-};
+export type Node =
+  | Block
+  | Comment
+  | Declaration
+  | FunctionCall
+  | MediaQuery
+  | Rule
+  | Selector
+  | SelectorList
+  | SelectorSection
+  | StyleSheet;
