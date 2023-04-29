a90a986ba676b939d3d82c81d3cec2e0001a2693
ej-shafran <ejshafran@hilma.tech>
2023-04-30 02:47:08 +0300

    Back to working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-// export * from "./parser";
-// export * from "./nodes";
-// export * from "./tokenize";
-
-import * as fs from "fs";
-import { parse } from "./parser";
-
-const filePath = "test.scss";
-const source = fs.readFileSync(filePath, "utf-8");
-
-const tree = parse(source, filePath);
-fs.writeFileSync("test.json", JSON.stringify(tree, null, 2));
+export * from "./parser";
+export * from "./nodes";
+export * from "./tokenize";
