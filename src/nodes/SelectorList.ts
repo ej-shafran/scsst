d38f42005cbc8ddfb6447b7bade2033b7b5e0926
ej-shafran <ejshafran@hilma.tech>
2023-05-03 00:26:18 +0300

    Update SelectorList

--- src/nodes/SelectorList.ts
+++ src/nodes/SelectorList.ts
@@ -1,10 +1,18 @@
-import { Loc } from "../tokenize";
+import { Loc, TokenOf } from "../tokenize";
 import { Selector } from "./Selector";
 
 export class SelectorList {
   readonly type = "SELECTOR_LIST";
 
-  constructor(public children: Selector[], public loc: Loc) { }
+  constructor(
+    public children: Selector[],
+    public loc: Loc,
+    private endToken: TokenOf<"OCURLY">
+  ) { }
+
+  getEndToken() {
+    return this.endToken;
+  }
 
   toString() {
     return this.children.map((child) => child.toString()).join(", ");
