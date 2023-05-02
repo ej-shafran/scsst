11d2d5db8b70c5e03c97d79f384c90f63e922aad
ej-shafran <ejshafran@hilma.tech>
2023-05-02 23:48:33 +0300

    Parse fix (4)

--- src/parser/parseDeclaration.ts
+++ src/parser/parseDeclaration.ts
@@ -1,85 +1,35 @@
 import { Declaration } from "../nodes/Declaration";
-import { FunctionCall } from "../nodes/FunctionCall";
 import { Value } from "../nodes/Value";
-import { Lexer, Token, TokenType, report } from "../tokenize";
+import { Lexer, TokenOf } from "../tokenize";
 import { ParserError } from "./ParserError";
 import { parseFunctionCall } from "./parseFunctionCall";
 
-export function parseDeclaration(lexer: Lexer, priorToken?: Token<"KEYWORD">) {
+export function parseDeclaration(lexer: Lexer, priorToken?: TokenOf<"KEYWORD">) {
   const keyToken = priorToken ?? lexer.expect("KEYWORD");
 
-  if (keyToken instanceof ParserError) {
-    report(keyToken.message, keyToken.loc);
-    return;
-  }
+  lexer.expect("COLON");
 
-  let error: Token<TokenType> | ParserError = lexer.expect("COLON");
+  const values: (Declaration["children"]) = [];
 
-  if (error instanceof ParserError) {
-    report(error.message, error.loc);
-    return;
-  }
-
-  let valueToken = lexer.expect("KEYWORD", "SPACE");
-
-  if (valueToken instanceof ParserError) {
-    report(valueToken.message, valueToken.loc);
-    return;
-  }
-
-  let semicolonOrFunc: Token<TokenType> | ParserError = lexer.expect(
-    "SEMICOLON",
-    "OPAREN",
-    "KEYWORD",
-    "SPACE"
-  );
-
-  if (semicolonOrFunc instanceof ParserError) {
-    report(semicolonOrFunc.message, semicolonOrFunc.loc);
-    return;
-  }
-
-  let hasInitialValue =
-    valueToken.type === "KEYWORD" && semicolonOrFunc.type !== "OPAREN";
-
-  const values: (Value | FunctionCall)[] = hasInitialValue
-    ? [new Value(valueToken.value, valueToken.loc)]
-    : [];
-
-  while (semicolonOrFunc.type !== "SEMICOLON") {
-    console.log(semicolonOrFunc);
-    if (semicolonOrFunc.type === "SPACE") {
-      semicolonOrFunc = lexer.expect("KEYWORD");
-
-      if (semicolonOrFunc instanceof ParserError) {
-        report(semicolonOrFunc.message, semicolonOrFunc.loc);
-        return;
-      }
-    }
-
-    if (semicolonOrFunc.type === "KEYWORD") {
-      if (hasInitialValue) {
-        hasInitialValue = false;
-      } else {
-        values.push(new Value(semicolonOrFunc.value, semicolonOrFunc.loc));
-      }
-    } else if (semicolonOrFunc.type === "OPAREN") {
-      const funcCall = parseFunctionCall(
-        lexer,
-        valueToken.value,
-        valueToken.loc,
-        semicolonOrFunc as Token<"OPAREN">
-      );
-      if (!funcCall) return;
-
-      values.push(funcCall);
-    }
-
-    semicolonOrFunc = lexer.expect("SEMICOLON", "OPAREN", "KEYWORD", "SPACE");
-
-    if (semicolonOrFunc instanceof ParserError) {
-      report(semicolonOrFunc.message, semicolonOrFunc.loc);
-      return;
+  let token: TokenOf<"KEYWORD" | "SPACE" | "SEMICOLON" | "OPAREN"> = lexer.expect("KEYWORD", "SPACE");
+  let latest: TokenOf<"KEYWORD"> | null = null;
+  while (token.type !== "SEMICOLON") {
+    token = lexer.expect("KEYWORD", "OPAREN", "SPACE", "SEMICOLON");
+    switch (token.type) {
+      case "KEYWORD":
+        if (latest) values.push(new Value(latest.value, latest.loc));
+        latest = token;
+        break;
+      case "OPAREN":
+        if (!latest) throw new ParserError("TODO", token.loc);
+        const funcCall = parseFunctionCall(lexer, latest.value, latest.loc, token);
+        values.push(funcCall);
+        latest = null;
+        break;
+      default:
+        if (latest) values.push(new Value(latest.value, latest.loc));
+        latest = null;
+        break;
     }
   }
 
