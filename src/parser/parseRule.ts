3f7539721c756c91ceea10f0fff352d6d2c07c43
ej-shafran <ejshafran@hilma.tech>
2023-05-03 00:27:58 +0300

    Parse fix (7)

--- src/parser/parseRule.ts
+++ src/parser/parseRule.ts
@@ -1,34 +1,17 @@
-import { Token, Lexer } from "../tokenize";
-
-import { SelectorList } from "../nodes";
+import { Lexer, TokenOf } from "../tokenize";
 import { Rule } from "../nodes/Rule";
-import { NESTED_SELECTOR_TOKENS, parseSelector } from "./parseSelector";
+import { SelectorToken, parseSelectorList } from "./parseSelectorList";
 import { parseBlock } from "./parseBlock";
+import { safe } from "./safe";
 
 export function parseRule(
   lexer: Lexer,
-  priorToken?: Token<(typeof NESTED_SELECTOR_TOKENS)[number]>,
+  priorToken?: TokenOf<SelectorToken>,
   isNested?: boolean
 ) {
-  let result = parseSelector(lexer, priorToken, isNested);
-  if (!result) return;
-
-  const originalLoc = result.selector.loc;
-
-  const selectors = [result.selector];
-  let token = result.token;
-
-  while (token.type !== "OCURLY") {
-    result = parseSelector(lexer, undefined, isNested);
-    if (!result) return;
-
-    selectors.push(result.selector);
-    token = result.token;
-  }
-
-  const selectorList = new SelectorList(selectors, originalLoc);
-  const block = parseBlock(lexer, token as Token<"OCURLY">);
-  if (!block) return;
-
-  return new Rule(selectorList, block, originalLoc);
+  const selectorList = parseSelectorList(lexer, priorToken, isNested);
+  const block = parseBlock(lexer, selectorList.getEndToken());
+  return new Rule(selectorList, block, selectorList.loc);
 }
+
+export default safe(parseRule);
