45f9a192029e9237d303964d0fc0bb9996f591c4
ej-shafran <ejshafran@hilma.tech>
2023-05-02 23:35:13 +0300

    Parse fix (2)

--- src/parser/parseComment.ts
+++ src/parser/parseComment.ts
@@ -1,18 +1,15 @@
 import { Comment } from "../nodes";
-import { Lexer, Token, report } from "../tokenize";
-import { ParserError } from "./ParserError";
+import { Lexer, TokenOf } from "../tokenize";
+import { safe } from "./safe";
 
 export function parseComment(
   lexer: Lexer,
-  priorToken?: Token<"SINGLE_LINE_COMMENT" | "BLOCK_COMMENT">
+  priorToken?: TokenOf<"SINGLE_LINE_COMMENT" | "BLOCK_COMMENT">
 ) {
   const token =
     priorToken ?? lexer.expect("SINGLE_LINE_COMMENT", "BLOCK_COMMENT");
 
-  if (token instanceof ParserError) {
-    report(token.message, token.loc);
-    return;
-  }
-
   return new Comment(token);
 }
+
+export default safe(parseComment);
