e38007e7ad316ce0d62c4233166afba360c1eead
ej-shafran <ejshafran@hilma.tech>
2023-05-02 23:34:45 +0300

    Parse fix (1)

--- src/parser/parseAtRule.ts
+++ src/parser/parseAtRule.ts
@@ -1,30 +1,19 @@
 import { AtRule } from "../nodes/AtRule";
-import { Lexer, Token, TokenType, report } from "../tokenize";
-import { ParserError } from "./ParserError";
+import { Lexer, TokenType, TokenOf } from "../tokenize";
+import { safe } from "./safe";
 
-export function parseAtRule(lexer: Lexer, priorToken?: Token<"KEYWORD">) {
-  let token: Token<TokenType> | ParserError =
-    priorToken ?? lexer.expect("KEYWORD");
-
-  if (token instanceof ParserError) {
-    report(token.message, token.loc);
-    return;
-  }
-
+export function parseAtRule(lexer: Lexer, priorToken?: TokenOf<"KEYWORD">) {
+  let token: TokenOf<TokenType> = priorToken ?? lexer.expect("KEYWORD");
   const originalLoc = token.loc;
 
   let content = "";
-
   while (token.type !== "SEMICOLON") {
     content += token.value;
 
     token = lexer.expect("KEYWORD", "SEMICOLON", "SPACE");
-
-    if (token instanceof ParserError) {
-      report(token.message, token.loc);
-      return;
-    }
   }
 
   return new AtRule(content, originalLoc);
 }
+
+export default safe(parseAtRule);
